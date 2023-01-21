141fc88c46f102b021d830120b8d3c87461db91f
JPachecoZ <jepach@gmail.com>
2023-01-21 07:11:14 -0500

    feat: connect mongoDB to the project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
-
 const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
     res.send('Welcome to my API')
+})
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log('Connected to MongoDB Atlas')
+})
+.catch((error) => {
+    console.error(error)
 })
 
 app.listen(port, () =>{
