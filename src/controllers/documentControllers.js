5e0cfdc97aab81faa9c37f758f2507ae00fe4df7
JPachecoZ <jepach@gmail.com>
2023-04-05 14:46:13 -0500

    test: sending data from bubble through quinny

--- src/controllers/documentControllers.js
+++ src/controllers/documentControllers.js
@@ -3,6 +3,34 @@ const jsonData = require('./test.json')
 const createSignRequest = async(req, res) => {
     console.log(">>>REQUEST BODY: ", req.body)
     console.log(">>>REQUEST HEADERS: ", req.headers)
+    const rawParticipantData = req.body.participants.split(',')
+    const dataToSend = {
+        type: 1,
+        subject: "FIRMA DE VENTA",
+        workflowId: 324,
+        participants: [
+            {
+                personByPersonId: {
+                    type: 1,
+                    documentType: rawParticipantData[0],
+                    documentNumber: rawParticipantData[1],
+                    firstname: rawParticipantData[2],
+                    lastname: rawParticipantData[3],
+                    email: rawParticipantData[4],
+                    cellphone: rawParticipantData[5],
+                },
+                orderParticipant: 1
+            }
+        ],
+        files: [
+            {
+                name: req.body.fileName,
+                base64: req.body.file
+            }
+        ]
+    }
+    console.log(">>>OBJETO STRING: ", JSON.stringify(dataToSend))
+
     try{
         const response = await fetch(`${process.env.SIGN_BASE_URL}${process.env.SIGN_CREATE_SIGN_REQUEST}`, {
             method: "POST",
@@ -10,7 +38,7 @@ const createSignRequest = async(req, res) => {
                 "Authorization": `${process.env.SIGN_AUTH_KEY}`,
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(jsonData)
+            body: JSON.stringify(dataToSend)
         })
         const data = await response.json()
         return res.status(200).json(data)
