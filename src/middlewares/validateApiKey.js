d6a9ff79e348671916a70edbb3dbd47d0d86e88a
JPachecoZ <jepach@gmail.com>
2023-04-05 14:29:00 -0500

    test: key

--- src/middlewares/validateApiKey.js
+++ src/middlewares/validateApiKey.js
@@ -1,4 +1,6 @@
 const validateApiKey = (req, res, next) => {
+  console.log(">>>HEADERS", req.headers)
+  console.log(">>>HEADERS", req.headers['Authorization'])
   const apiKey = req.headers['Authorization'];
   if (!apiKey || apiKey !== process.env.API_KEY_QUINNY) {
     return res.status(401).json({ message: 'API key inválida' });
